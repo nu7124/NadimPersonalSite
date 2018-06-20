5f453a73ee14329716c5e751cd7ab04f793c36fb
nu7124 <nadim.uddin1995@gmail.com>
2018-06-20 17:05:04 -0400

    added title text component

---
+++ src/components/Title/title.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './title.css';
+
+class title extends React.Component {
+    render() {
+      return (
+        <div id="title">
+            <h1 id="title-name">Nadim Uddin</h1>
+        </div>
+      );
+    }
+}
+
+export default title;
