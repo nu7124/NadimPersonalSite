11cfe6d4b0d1a68ad2d6516d9ba002e854bb7535
nu7124 <nadim.uddin1995@gmail.com>
2018-06-20 15:44:29 -0400

    starting over

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './index.css';
 
-import Main from './components/main/main.js'
-
 class home extends React.Component {
     render() {
       return (
         <div id="home">
-          <Main />
+          <div id="test">
+          </div>
+          <div id="test2"></div>
         </div>
       );
     }
