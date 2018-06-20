3de307ffbafedbc78ef0118e3a4ff4ace0155b3d
nu7124 <nadim.uddin1995@gmail.com>
2018-06-20 17:06:15 -0400

    experiemnting with colors, got slant on div

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './index.css';
 
+import Title from '../components/Title/title';
+
 class home extends React.Component {
     render() {
       return (
         <div id="home">
           <div id="test">
+            <Title />
           </div>
-          <div id="test2"></div>
+          <div id="test2">
+          </div>
         </div>
       );
     }
