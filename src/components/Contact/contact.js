83e0241a450ec532893a42d93857c882bb1d6f78
nu7124 <nadim.uddin1995@gmail.com>
2018-06-25 17:28:38 -0400

    added Contact component, not finished

---
+++ src/components/Contact/contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './contact.css';
+
+class contact extends React.Component {
+    render() {
+      return (
+        <div id="contactComp">
+            <h1 id="contactComp_name">Contact</h1>
+        </div>
+      );
+    }
+}
+
+export default contact;
