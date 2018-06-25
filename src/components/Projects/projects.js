9d6da1435caea16ac807950903016684c4914b0f
nu7124 <nadim.uddin1995@gmail.com>
2018-06-25 16:22:13 -0400

    added projects components files, not finished

---
+++ src/components/Projects/projects.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './projects.css';
+
+class projects extends React.Component {
+    render() {
+      return (
+        <div id="projectsComp">
+            
+        </div>
+      );
+    }
+}
+
+export default projects;
