e43e7fbb7966e7303b235623a5257bdc50b80c34
nu7124 <nadim.uddin1995@gmail.com>
2018-06-25 16:23:14 -0400

    added project component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import './index.css';
 
 import Title from '../components/Title/title';
 import About from '../components/About/about';
+import Projects from '../components/Projects/projects';
 
 class home extends React.Component {
     render() {
@@ -11,9 +12,15 @@ class home extends React.Component {
           <div id="main">
             <Title />
           </div>
+
           <div id="about">
             <About />
           </div>
+
+          <div id="projects">
+            <Projects />
+          </div>
+
         </div>
       );
     }
