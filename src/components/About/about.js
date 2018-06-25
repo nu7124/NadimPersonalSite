1b0e0a54e73164bd4b9cb042ce1cb04eed498621
nu7124 <nadim.uddin1995@gmail.com>
2018-06-25 14:33:48 -0400

    added about component files, not finished yet

---
+++ src/components/About/about.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './about.css';
+
+class about extends React.Component {
+    render() {
+      return (
+        <div id="aboutComp">
+            <div>h</div>
+            <div>h</div>
+            <div>h</div>
+        </div>
+      );
+    }
+}
+
+export default about;
