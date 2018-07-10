9b5c69b48c7f1957cd8d26c368b6d3bc805d616c
nu7124 <nadim.uddin1995@gmail.com>
2018-07-10 02:51:32 -0400

    more style changes

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './skills.css';
-const front=["JavaScript/ES6+","React","HTML","CSS","React-Native","Redux","Grid","Webpack"];
+const front=["JavaScript/ES6+","React","HTML","CSS","React-Native","Redux","Grid","Webpack", "AJAX"];
 const back=["Express","Sequelize","Node","Python","PostgreSQL","Firebase"];
 const other=["Git","Mocha", "Chai", "Postman", "Agile Workflow"];
 const skills = ()=>{
@@ -8,12 +8,12 @@ const skills = ()=>{
     <div id="skillsComp">
       <h1 id="skillsComp_name">Skills</h1>
       <div className="skillsComp_set">
-        <div className="skillsComp_set_title">Front End</div>
+        <div className="skillsComp_set_title" id="front"><div className="inner">Front End</div></div>
         <div className="skillsComp_set_individual">
           {
             front.map(skill=>{
               return (
-                <span className="single_skill" key={skill}>{skill} </span>
+                <span className="single_skill" key={skill}>{skill}</span>
               )
             })
           }
@@ -21,7 +21,7 @@ const skills = ()=>{
       </div>
 
       <div className="skillsComp_set">
-        <div className="skillsComp_set_title">Back End</div>
+        <div className="skillsComp_set_title" id="back"><div className="inner">Back End</div></div>
         <div className="skillsComp_set_individual">
           {
             back.map(skill=>{
@@ -34,7 +34,7 @@ const skills = ()=>{
       </div>
 
       <div className="skillsComp_set">
-        <div className="skillsComp_set_title">Other</div>
+        <div className="skillsComp_set_title" id="other"><div className="inner">Other</div></div>
         <div className="skillsComp_set_individual">
           {
             other.map(skill=>{
