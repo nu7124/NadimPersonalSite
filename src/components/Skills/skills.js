e4f70f798f5d0ba3bc229418685e006cc822e30b
nu7124 <nadim.uddin1995@gmail.com>
2018-07-09 22:59:16 -0400

    added skills background image, render skills on skill page

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -1,10 +1,50 @@
 import React from 'react';
 import './skills.css';
-
+const front=["JavaScript/ES6+","React","HTML","CSS","React-Native","Redux","Grid","Webpack"];
+const back=["Express","Sequelize","Node","Python","PostgreSQL","Firebase"];
+const other=["Git","Mocha", "Chai", "Postman", "Agile Workflow"];
 const skills = ()=>{
   return (
     <div id="skillsComp">
       <h1 id="skillsComp_name">Skills</h1>
+      <div className="skillsComp_set">
+        <div className="skillsComp_set_title">Front End</div>
+        <div className="skillsComp_set_individual">
+          {
+            front.map(skill=>{
+              return (
+                <span className="single_skill" key={skill}>{skill} </span>
+              )
+            })
+          }
+        </div>
+      </div>
+
+      <div className="skillsComp_set">
+        <div className="skillsComp_set_title">Back End</div>
+        <div className="skillsComp_set_individual">
+          {
+            back.map(skill=>{
+              return (
+                <span className="single_skill" key={skill}>{skill} </span>
+              )
+            })
+          }
+        </div>
+      </div>
+
+      <div className="skillsComp_set">
+        <div className="skillsComp_set_title">Other</div>
+        <div className="skillsComp_set_individual">
+          {
+            other.map(skill=>{
+              return (
+                <span className="single_skill" key={skill}>{skill} </span>
+              )
+            })
+          }
+        </div>
+      </div>
     </div>
   );
 }
