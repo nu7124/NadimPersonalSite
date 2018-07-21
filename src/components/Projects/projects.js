0478d5f1f743124cf7c3937ee431f1e3298bdf84
nu7124 <nadim.uddin1995@gmail.com>
2018-07-21 01:35:05 -0400

    setup for projects page

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -4,7 +4,12 @@ import './projects.css';
 const projects = ()=>{
   return (
     <div id="projectsComp">
-      <h1 id="projectsComp_name">Projects</h1>
+      <h1 id="projectsComp_name">Portfolio</h1>
+      <div id="projectsComp_all">
+        <div className="projectsComp_all_individual" style={{backgroundColor:'red'}}></div>
+        <div className="projectsComp_all_individual" style={{backgroundColor:'blue'}}></div>
+        <div className="projectsComp_all_individual" style={{backgroundColor:'green'}}></div>
+      </div>
     </div>
   );
 }
